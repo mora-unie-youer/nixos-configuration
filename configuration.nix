2304a5f432660b3295c7c7f4a53bdff4038dfe30
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-03-18 05:21:10 +0000

    Remove bootloader, for now

--- configuration.nix
+++ configuration.nix
@@ -10,9 +10,6 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
