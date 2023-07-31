2e7a636fb171bb577b25f9ffe33bda5f9141bfff
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-07-31 18:23:32 +0000

    Made some basic `core` system module

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -1,8 +1,39 @@
-_:
+{ pkgs, ... }:
 
 {
   # Setting up core features in NixOS configuration
   config = {
-    # Huh
+    # Setting up default locale to en_US.UTF-8
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    # Setting up system timezone to UTC
+    time.timeZone = "UTC";
+
+    # Setting up global (system) environment
+    environment = {
+      # Including some packages into system environment
+      #
+      # Should be used for packages that I use the most at any user
+      # or everytime install using `nix-shell -p`
+      systemPackages = with pkgs; [
+        # ~~Just to look cool~~
+        neofetch
+      ];
+    };
+
+    # Setting up really important services
+    services = {
+      # Enabling fwupd
+      fwupd.enable = true;
+
+      # Enabling udisks2
+      udisks2.enable = true;
+    };
+
+    # Enabling some disabled documentation
+    documentation = {
+      dev.enable = true;
+      man.generateCaches = true;
+    };
   };
 }
