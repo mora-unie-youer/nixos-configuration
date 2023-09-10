888ca4093cf9f70be2057e8641a350d7c9de49f7
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-09-10 12:57:51 +0000

    feat: set up `mora` user a little bit

--- modules/user/common/default.nix
+++ modules/user/common/default.nix
@@ -1,6 +1,18 @@
-_:
+{ pkgs, ... }:
 
 {
   # Importing everything related to common user
   imports = [];
+
+  # Configuring home-manager
+  home = {
+    # Setting some state version
+    stateVersion = "23.11";
+
+    # "Global" environment variables
+    sessionVariables = {};
+
+    # "Global" packages
+    packages = with pkgs; [];
+  };
 }
--- modules/user/mora/default.nix
+++ modules/user/mora/default.nix
@@ -1,6 +1,19 @@
-_:
+{ pkgs, ... }:
 
 {
   # Importing everything related to "mora" user
   imports = [];
+
+  # Configuring home-manager
+  home = {
+    # Setting some user information
+    username = "mora";
+    homeDirectory = "/home/mora";
+
+    # "Local" environment variables
+    sessionVariables = {};
+
+    # "Local" packages
+    packages = with pkgs; [];
+  };
 }
