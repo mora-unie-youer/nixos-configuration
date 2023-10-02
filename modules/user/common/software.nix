25f250c213869d3ed59c7fde0184e3273a4ef0ce
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-10-02 14:14:49 +0000

    feat: add shells to common user

--- modules/user/common/software.nix
+++ modules/user/common/software.nix
@@ -5,5 +5,26 @@
   config = {
     # "Global" packages
     home.packages = with pkgs; [];
+
+    # Configuring programs
+    programs = {
+      # Configuring Home Manager
+      home-manager.enable = true;
+
+      # Configuring Bash
+      bash.enable = true;
+
+      # Configuring Fish
+      fish.enable = true;
+
+      # Configuring Ion
+      ion.enable = true;
+
+      # Configuring Nushell
+      nushell.enable = true;
+
+      # Configuring ZSH
+      zsh.enable = true;
+    };
   };
 }
