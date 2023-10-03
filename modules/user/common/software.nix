2d8b2667aacafb10bc4ffeb80def333e34b6863e
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-10-03 07:44:44 +0000

    feat: set up starship and zellij auto-start

--- modules/user/common/software.nix
+++ modules/user/common/software.nix
@@ -38,7 +38,34 @@
       bash.enable = true;
 
       # Configuring Fish
-      fish.enable = true;
+      fish = {
+        enable = true;
+        interactiveShellInit = ''
+          # The following snippet is meant to be used like this in your fish config:
+          #
+          # if status is-interactive
+          #     # Configure auto-attach/exit to your likings (default is off).
+          #     # set ZELLIJ_AUTO_ATTACH true
+          #     # set ZELLIJ_AUTO_EXIT true
+          #     eval (zellij setup --generate-auto-start fish | string collect)
+          # end
+
+          set ZELLIJ_AUTO_EXIT true
+          if not set -q ZELLIJ
+            if not string match -q "/dev/tty*" (tty)
+              if test "$ZELLIJ_AUTO_ATTACH" = "true"
+                zellij attach -c
+              else
+                zellij
+              end
+
+              if test "$ZELLIJ_AUTO_EXIT" = "true"
+                kill $fish_pid
+              end
+            end
+          end
+        '';
+      };
 
       # Configuring Ion
       ion.enable = true;
@@ -53,7 +80,7 @@
       zellij = {
         enable = true;
         enableBashIntegration = true;
-        enableFishIntegration = true;
+        # enableFishIntegration = true;
         enableZshIntegration = true;
       };
 
@@ -61,6 +88,16 @@
       atuin = {
         enable = true;
         flags = [ "--disable-up-arrow" ];
+      };
+
+      # Configuring Starship (pretty prompt)
+      starship = {
+        enable = true;
+        enableBashIntegration = true;
+        enableFishIntegration = true;
+        enableIonIntegration = true;
+        enableNushellIntegration = true;
+        enableZshIntegration = true;
       };
 
       # Configuring FZF
