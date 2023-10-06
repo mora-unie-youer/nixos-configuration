5a8d332a60cbaa68376743ca5b71976f66e5269a
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-10-06 15:45:51 +0000

    feat(mora): configure GnuPG

--- modules/user/mora/software.nix
+++ modules/user/mora/software.nix
@@ -25,12 +25,36 @@
     programs = {
       # Configuring foot terminal
       foot.enable = true;
+
+      # Configuring GnuPG
+      gpg = {
+        enable = true;
+
+        settings = {
+          ask-cert-level = true;
+          expert = true;
+
+          list-options = [ "show-sig-expire" "show-uid-validity" "show-unusable-subkeys" "show-unusable-uids"];
+          verify-options = [ "show-uid-validity" "show-unusable-uids" ];
+
+          # with-fingerprint twice
+          with-fingerprint = [ "" "" ];
+          with-keygrip = true;
+        };
+      };
     };
 
     # Configuring services
     services = {
       # Enabling Gnome Keyring
       gnome-keyring.enable = true;
+
+      # Configuring GnuPG agent
+      gpg-agent = {
+        enable = true;
+        enableSshSupport = true;
+        pinentryFlavor = "gnome3";
+      };
     };
   };
 }
