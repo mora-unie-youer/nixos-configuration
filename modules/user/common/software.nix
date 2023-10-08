e61b6b56d087dfa68adc3dac14270c249570eb8d
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-10-08 12:42:18 +0000

    feat: remove zellij autostart from common user

--- modules/user/common/software.nix
+++ modules/user/common/software.nix
@@ -39,34 +39,7 @@
       bash.enable = true;
 
       # Configuring Fish
-      fish = {
-        enable = true;
-        interactiveShellInit = ''
-          # The following snippet is meant to be used like this in your fish config:
-          #
-          # if status is-interactive
-          #     # Configure auto-attach/exit to your likings (default is off).
-          #     # set ZELLIJ_AUTO_ATTACH true
-          #     # set ZELLIJ_AUTO_EXIT true
-          #     eval (zellij setup --generate-auto-start fish | string collect)
-          # end
-
-          set ZELLIJ_AUTO_EXIT true
-          if not set -q ZELLIJ
-            if not string match -q "/dev/tty*" (tty)
-              if test "$ZELLIJ_AUTO_ATTACH" = "true"
-                zellij attach -c
-              else
-                zellij
-              end
-
-              if test "$ZELLIJ_AUTO_EXIT" = "true"
-                kill $fish_pid
-              end
-            end
-          end
-        '';
-      };
+      fish.enable = true;
 
       # Configuring Ion
       ion.enable = true;
@@ -78,12 +51,7 @@
       zsh.enable = true;
 
       # Configuring zellij
-      zellij = {
-        enable = true;
-        enableBashIntegration = true;
-        # enableFishIntegration = true;
-        enableZshIntegration = true;
-      };
+      zellij.enable = true;
 
       # Configuring Atuin (Ctrl-R in shell)
       atuin = {
