150883d49e686f3b1eb23031253953912793b9ca
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-10-27 16:52:14 +0000

    fix(mora): stop using swaylock

--- modules/user/mora/swayidle.nix
+++ modules/user/mora/swayidle.nix
@@ -29,20 +29,9 @@
     enable = true;
     systemdTarget = "hyprland-session.target";
 
-    events = [
-      {
-        event = "before-sleep";
-        command = "${pkgs.swaylock-effects}/bin/swaylock";
-      }
-    ];
-
     timeouts = [
       {
         timeout = 300;
-        command = "${pkgs.swaylock-effects}/bin/swaylock";
-      }
-      {
-        timeout = 420;
         command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
         resumeCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       }
