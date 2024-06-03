3fc2757bd4cce60d4c2a32a8641fd4c81beacec6
Mora Unie Youer <mora_unie_youer@riseup.net>
2024-06-03 09:49:42 +0000

    refactor: move yggdrasil installation to common system config

--- system/sapphire/default.nix
+++ system/sapphire/default.nix
@@ -71,52 +71,6 @@
   services = {
     flatpak.enable = true;
     resolved.enable = true;
-
-    yggdrasil = {
-      enable = true;
-      openMulticastPort = true;
-      configFile = ../../secrets/yggdrasil-sapphire.conf;
-      settings = {
-        Peers = [
-          "tcp://ygg-msk-1.averyan.ru:8363"
-          "tls://ygg-msk-1.averyan.ru:8362"
-          "quic://ygg-msk-1.averyan.ru:8364"
-
-          # sergeysedoy97, x-mow-1, 200 Mbit/s, Dual-Stack (средний пинг 4ms)
-          "tcp://185.103.109.63:65533"
-          "tcp://[2a09:5302:ffff::ac9]:65533"
-          "tls://185.103.109.63:65534"
-          "tls://[2a09:5302:ffff::ac9]:65534"
-          "quic://185.103.109.63:65535"
-          "quic://[2a09:5302:ffff::ac9]:65535"
-
-          # sergeysedoy97, x-mow-2, 100 Mbit/s, Dual-Stack (средний пинг 4ms)
-          "tcp://45.95.202.91:65533"
-          "tcp://[2a09:5302:ffff::aca]:65533"
-          "tls://45.95.202.91:65534"
-          "tls://[2a09:5302:ffff::aca]:65534"
-          "quic://45.95.202.91:65535"
-          "quic://[2a09:5302:ffff::aca]:65535"
-
-          # sergeysedoy97, x-mow-3, 100 Mbit/s, Dual-Stack (средний пинг 4ms)
-          "tcp://[2a00:b700::c:2e1]:65533"
-          "tls://[2a00:b700::c:2e1]:65534"
-          "quic://[2a00:b700::c:2e1]:65535"
-
-          # sergeysedoy97, x-mow-4, 100 Mbit/s, Dual-Stack (средний пинг 5ms)
-          "tcp://[2a00:b700:3::3:3b4]:65533"
-          "tls://[2a00:b700:3::3:3b4]:65534"
-          "quic://[2a00:b700:3::3:3b4]:65535"
-
-          # sergeysedoy97, x-mow-0, 1 Gbit/s, Dual-Stack (средний пинг 15ms)
-          "tcp://178.20.41.3:65533"
-          "tcp://[2a0d:8480:2:6bd::]:65533"
-          "tls://178.20.41.3:65534"
-          "tls://[2a0d:8480:2:6bd::]:65534"
-          "quic://178.20.41.3:65535"
-          "quic://[2a0d:8480:2:6bd::]:65535"
-        ];
-      };
-    };
+    yggdrasil.configFile = ../../secrets/yggdrasil-sapphire.conf;
   };
 }
